b2b83f573ca5a9be0486d2d37d09173284d324ca
zteeed <zteeed@minet.net>
2020-01-08 19:56:06 +0000

    Enable html message inside sweetalert2 flash message

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -2,15 +2,6 @@ function display_sw2(msg, title, type) {
   Swal.fire({
       type: type,
       title: title,
-      text: msg,
-  })
-}
-
-
-function display_sw2_html(msg, title, type) {
-  Swal.fire({
-      type: type,
-      title: title,
       html: msg,
   })
 }
