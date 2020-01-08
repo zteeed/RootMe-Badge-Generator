909571764c6d1725a40aed13c347a687d60f8327
zteeed <zteeed@minet.net>
2020-01-08 20:20:16 +0000

    Fix data parsing depending on api data keys

--- src/parser.py
+++ src/parser.py
@@ -1,20 +1,23 @@
 from typing import Dict
 
+from src.http_client import RMAPI
 
-def extract_data(data: Dict, url: str) -> Dict:
-    top = 100 * data["ranking"] / data["ranking_tot"]
+
+def extract_data(data: Dict, id_auteur: int, api: RMAPI, url: str) -> Dict:
+    top = 100 * data['position'] / api.number_users
     top = '{0:.2f}'.format(top)
     return {
         'url': url,
-        'name': data['pseudo'],
-        'avatar_url': data['avatar_url'],
+        'name': data['nom'],
+        'fullname': f'{data["nom"]}-{id_auteur}',
+        'avatar_url': api.get_avatar_url(data['nom'], id_auteur),
         'score': data['score'],
-        'rank': data['ranking_category'],
-        'ranking': data['ranking'],
-        'ranking_tot': data['ranking_tot'],
+        'rank': api.get_rank(data['nom'], id_auteur),
+        'ranking': data['position'],
+        'ranking_tot': api.number_users,
         'top': f'{top}%',
         'challenge': {
-            'solved': data['nb_challenges_solved'],
-            'total': data['nb_challenges_tot']
+            'solved': len(data['validations']),
+            'total': api.number_challenges
         }
     }
