12d12eb0596d0474570d630d6ea538abf4dc9553
zteeed <aurelienduboc96@gmail.com>
2021-05-06 16:33:48 +0200

    Fix get_rank xpath parsing

--- src/http_client.py
+++ src/http_client.py
@@ -227,10 +227,9 @@ class RMAPI:
         return f'{self.api_url}/{result[0]}'
 
     def get_rank(self, profile_page_url: str) -> str:
-        content = self.http_get(profile_page_url + '?inc=score')
+        content = self.http_get(profile_page_url)
         tree = html.fromstring(content)
-        result = tree.xpath('//div[@class="t-body tb-padding"]/div/div[@class="row text-center"]/div/span/text()')
-        result = [item.strip() for item in result if item.strip()]
+        result = tree.xpath('//tr[@style="border: 1px #2ba6cb solid"]/td[@class="show-for-large-up"]')[-1].text
         if not result:
             return 'newbie'
-        return result[-1]
+        return result.strip()
